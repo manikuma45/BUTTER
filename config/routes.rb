7b09b0ff27439138e1ad5780b6251a111bc2b117
manish kumar <manikuma0405@gmail.com>
2020-05-23 12:59:58 +0900

    root toの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+root to:'bing_search#index'
 
-	resources :bing_search, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :bing_search, only: [:index]
 end
